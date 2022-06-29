4620e4b45d2cb191d5b33269c536780ca1c33b60
mendoza099 <sergiomendozadominguez@gmail.com>
2022-06-29 15:59:07 +0000

    traficht

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import "../styles/index.scss";
 
 //import your own components
-import Home from "./component/home.jsx";
+
+import TrafficLight from "./TrafficLight.jsx"
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
